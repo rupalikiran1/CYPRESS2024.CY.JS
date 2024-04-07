7c9b4c498d07b20774e5fcb807ced64aeec47a48
rupalikiran1 <rupalikiran1999@gmail.com>
2024-04-07 09:35:55 +0530

    added

---
+++ cypress/e2e/waiting.cy.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+context('waiting',() => {
+    beforeEach(() => {
+        cy.visit('https://example.cypress.io/commands/waiting')
+    })
+    it('cy.wait() - wait for a specific amount of time', () => {
+        cy.get('.wait-input1').type('Wait 1000ms after typing')
+        cy.wait(1000)
+        cy.get('.wait-input2').type('Wait 1000ms after typing')
+        cy.wait(1000)
+        cy.get('.wait-input3').type('Wait 1000ms after typing')
+        cy.wait(1000)
+    })
+    it('cy.wait() - wait for a specific route',() => {
+        cy.intercept('GET', '**/comments/*').as('getComment')
+        cy.get('.network-btn').click()
+        cy.wait('@getComment').its('response.statusCode').should('be.oneOf', [200, 304])
+
+    })
+})
