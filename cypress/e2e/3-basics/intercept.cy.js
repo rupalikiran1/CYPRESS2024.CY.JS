158c077940375fdb6053f297201d8bda36263f1e
rupalikiran1 <rupalikiran1999@gmail.com>
2024-05-03 15:27:59 +0530

    added

---
+++ cypress/e2e/3-basics/intercept.cy.js
@@ -0,0 +1,41 @@
+/// <reference types="cypress"/>
+
+describe('intercept', function () {
+    it.only('wait with cy.intercept', function () {
+        cy.intercept({ url: "https://jsonplaceholder.cypress.io/comments/1", method: "GET" }).as('GetComment')
+        cy.visit('https://example.cypress.io/commands/network-requests')
+        cy.contains('GetComment').click()
+        cy.wait('@GetComment')
+        cy.get('.network-comment').should('contain', 'laudantium')
+    })
+
+    it.only('wait with cy.intercept and data mapping', function () {
+        cy.intercept({ url: "https://jsonplaceholder.cypress.io/comments/1", method: "GET" }).as('GetComment')
+        cy.visit('https://example.cypress.io/commands/network-requests')
+        cy.contains('GetComment').click()
+        cy.wait('@GetComment').then(function ({ response, request }) {
+            cy.log(response.body.body)
+            cy.get('.network-comment').should('have.text', response.body.body)
+        })
+    })
+    it.only('wait with cy.intercept and stub network with mock', function () {
+        cy.intercept({ url: "https://jsonplaceholder.cypress.io/comments/1", method: "GET" },
+            {
+                body: {
+                    "postId": 1,
+                    "id": 1,
+                    "name": "id labore ex et quam laborum",
+                    "email": "Eliseo@gardner.biz",
+                    "body": "Rupali"
+                }
+            }
+        ).as('GetComment')
+        cy.visit('https://example.cypress.io/commands/network-requests')
+        cy.contains('GetComment').click()
+        cy.wait('@GetComment').then(function ({ response, request }) {
+            cy.log(response.body.body)
+            cy.get('.network-comment').should('have.text', response.body.body)
+        })
+
+    })
+})
