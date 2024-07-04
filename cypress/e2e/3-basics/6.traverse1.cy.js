6c0cb6f988b8f4ad54d039bf65ef0f2b7feed411
rupalikiran1 <rupalikiran1999@gmail.com>
2024-07-04 09:18:40 +0530

    first commit

--- cypress/e2e/3-basics/6.traverse1.cy.js
+++ cypress/e2e/3-basics/6.traverse1.cy.js
@@ -54,9 +54,16 @@ describe('verify DOM element using various functions', function () {
         cy.get('#form-textfield').find('[name="firstname"]').type('rupali')
         cy.get('#form-textfield').find('[name="lastname"]').type('kotkar')
     })
-    it.only('to get DOM element within element using .closest()', function () {
+    it('to get DOM element within element using .closest()', function () {
         cy.get('[name="firstname"]').closest('.thumbnail').should('have.attr', 'id', 'thumbnail-1')
         cy.get('.navbar-toggle').closest('.navbar').should('have.attr', 'role', 'navigation')
+    })
+    it.only('to get DOM element within element using .closest()',function(){
+        cy.get('#fruits').parents()
+        cy.get('#fruits').parents().should('have.length',6)
+        cy.get('#fruits').parents().should('match','body')
+        cy.get('#fruits').parents().should('match','[class="container"]')
+        cy.get('#fruits').parents().should('match','[class="col-sm-12"]')
     })
 })
 
